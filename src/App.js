521ee5d74d7be1bc9c65fbeca69c1420f49cd7f5
Rebecca Tiessen <becca.tiessen@gmail.com>
2020-07-18 20:00:12 -0500

    cleanup and adding grommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import './App.css';
-import GarageMap from './pages/GarageMap';
+import {Grommet} from 'grommet';
+import Home from './pages/Home/Home';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+    colors: {
+      brand: '#FFBD71',
+      accent1: '#C73866',
+      accent2: '#FE6763',
+      accent3: '#FD8F52',
+      accent4: '#FFDCA2',
+    },
+  },
+};
 
 function App() {
   return (
-    <GarageMap/>
+    <Grommet theme={theme} full>
+      <Home/>
+    </Grommet>
   );
 }
 
---
+++ src/pages/GarageMap/GarageMap.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+const GarageMap = ({google}) => {
+    // const [showWindow, setShowWindow] = useState(false);
+    // const [activeMarker, setActiveMarker] = useState({});
+    // const [selectedPlace, setSelectedPlace] = useState({});
+
+    // const onMarkerClick = (props, marker) => {
+    //   setShowWindow(true);
+    //   setActiveMarker(marker);
+    //   setSelectedPlace(props);
+    // }
+
+    // const onClose = props => {
+    // if (this.state.showingInfoWindow) {
+    //     this.setState({
+    //         showingInfoWindow: false,
+    //         activeMarker: null
+    //         });
+    //     }
+    // };
+
+      return (
+        <Map
+          google={google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+           lat: 49.8844,
+           lng: -97.14704,
+          }}
+        >
+            {/* <Marker onClick={onMarkerClick()} name={`Placeholder name`}/>
+            <InfoWindow
+            marker={activeMarker}
+            visible={showWindow}
+            onClose={onClose}
+            >
+                <div>
+                    <h4>{selectedPlace.name}</h4>
+                </div>
+            </InfoWindow> */}
+        </Map>
+      );
+}
+  
+  export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDyy_4tCE_t4Qf_HYvc_aFBs0Ic38-xfjc'
+  })(GarageMap);
+
+  const mapStyles = {
+    width: '100%',
+    height: '100%'
+  };
